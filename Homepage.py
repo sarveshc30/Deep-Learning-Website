8d15cf12bd050dc2f0f814601de437caa193225d
sarveshc30 <64366184+sarveshc30@users.noreply.github.com>
2025-04-03 12:53:40 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -6,8 +6,7 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from pickle import load
 
-st.set_page_config(layout="wide")
-st.markdown("<h1 style='text-align: center; font-size: 40px;'>Home Page</h1>", unsafe_allow_html=True)
+
 
 # Initialize session state keys if not already set
 if "logged_in" not in st.session_state:
@@ -25,7 +24,7 @@ db = client.webdb
 people = db.people
 
 
-#instert user in dictionary format
+#checks if user exists and checks password in Mongo Database
 def validate_user(input_email, input_password):
     user = people.find_one({"email": input_email})
     if user and user["password"] == str(hash(input_password)):
@@ -34,6 +33,7 @@ def validate_user(input_email, input_password):
 
 # Login Page
 if st.session_state.page == "login":
+    st.set_page_config(layout="centered")
     st.subheader("Login")
     email = st.text_input("Email")
     password = st.text_input("Password", type="password")
@@ -58,6 +58,7 @@ elif st.session_state.page == "sign_up":
     password = st.text_input("Password", type="password")
     password2 = st.text_input("Repeat Password", type="password")
 
+    #inserts user  if doesn't exist
     if st.button("Sign Up"):
         if password2 == password:
             if people.find_one({"email": email}):
@@ -72,6 +73,8 @@ elif st.session_state.page == "sign_up":
 
 # Home Page with project options
 elif st.session_state.page == "home":
+    st.set_page_config(layout="wide")
+    st.markdown("<h1 style='text-align: center; font-size: 40px;'>Home Page</h1>", unsafe_allow_html=True)
     col1, col2, col3 = st.columns(3, gap="large")
     with col1:
         st.info("""
@@ -109,13 +112,16 @@ elif st.session_state.page == "home":
 elif st.session_state.page == "spam":
     if Spam_Detection.spam():
         st.session_state.page = "home"
+        st.rerun()
 
 # Movie Classification Page
 elif st.session_state.page == "movie":
     if Movie_Classification.movie():
         st.session_state.page = "home"
+        st.rerun()
 
 # Customer Churn Page
 elif st.session_state.page == "churn":
     if Customer_Churn.churn():
         st.session_state.page = "home"
+        st.rerun()
